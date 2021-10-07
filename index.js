e99f4060722cc930402a2b71f7638fa15fc0bdc4
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 19:15:42 -0300

    adding route to index.js file a return one user

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const bodyparser = require('body-parser');
 
 const addUser = require('./routers/AddUser');
 const getAllUser = require('./routers/GetUsers');
+const getUser = require('./router/getUser');
 
 app.use(bodyparser.json());
 
 app.use('/user', addUser);
 app.use('/users', getAllUser);
+app.use('/user', getUser);
 
 const PORT = process.env.PORT || 3000;
 
