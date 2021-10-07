aa0d37fbf56f9d995e495d9dc57b01c53b145a49
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 20:01:40 -0300

    adding validations

--- routers/GetUser.js
+++ routers/GetUser.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 const { getUserById } = require('../model/User');
 
-router.get('/:id', async (req, res) => {
+const validateUser = async (req, res, next) => {
+  const { id } = req.params;
+  const response = await getUserById(id);
+  if(!response) return res.status(401).json({ message: 'user not found!' });
+  next();
+};
+
+router.get('/:id', validateUser, async (req, res) => {
   const { id } = req.params;
   const response = await getUserById(id);
   res.status(200).json(response);
