af823bc083d08832fc6f483fb46b0aacba35e068
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 15:59:14 -0300

    adding script

--- middlewares/User.js
+++ middlewares/User.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.router();
 
-router.post('/', async (req, res) => {
+const {
+  create,
+} = require('../model/User');
+
+router.post('/user', async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
+  const user = await create(firstName, lastName, email, password);
+
+  res.status(201).json({ user })
 });
 
 module.exports = router;
