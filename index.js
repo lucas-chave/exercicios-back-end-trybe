a0bd80e57ae9d595320ff9d070c5df749c43e343
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 21:32:58 -0300

    adding route for user update

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const bodyparser = require('body-parser');
 
 const addUser = require('./routers/AddUser');
 const getAllUser = require('./routers/GetUsers');
-const getUse = require('./routers/GetUser');
+const getUser = require('./routers/GetUser');
+const updateUser = require('./routers/UpdateUser');
 
 app.use(bodyparser.json());
 
 app.use('/user', addUser);
 app.use('/users', getAllUser);
-app.use('/user', getUse);
+app.use('/user', getUser);
+app.use('/user', updateUser);
 
 const PORT = process.env.PORT || 3000;
 
