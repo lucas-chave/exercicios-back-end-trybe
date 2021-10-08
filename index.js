d2ac0f27386be672e0cf7e54f78a26e133809b37
lucas-chave <lcchavesr150@gmail.com>
2021-10-07 22:41:09 -0300

    fix user delete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const addUser = require('./routers/AddUser');
 const getAllUser = require('./routers/GetUsers');
 const getUser = require('./routers/GetUser');
 const updateUser = require('./routers/UpdateUser');
+const deleteUser = require('./routers/DeleteUser');
 
 app.use(bodyparser.json());
 
@@ -13,6 +14,7 @@ app.use('/user', addUser);
 app.use('/users', getAllUser);
 app.use('/user', getUser);
 app.use('/user', updateUser);
+app.use('/user', deleteUser);
 
 const PORT = process.env.PORT || 3000;
 
--- routers/DeleteUser.js
+++ routers/DeleteUser.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { deleteUserById } = require('../model/User');
+
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   const response = await deleteUserById(id);
