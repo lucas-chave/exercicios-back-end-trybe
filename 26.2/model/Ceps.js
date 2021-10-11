504b9b2d6c9b2e8e0ae32d2d49078020ea736c0e
lucas-chave <lcchavesr150@gmail.com>
2021-10-11 17:12:13 -0300

    adding function for get all ceps

--- 26.2/model/Ceps.js
+++ 26.2/model/Ceps.js
@@ -1,18 +1,11 @@
-const { MongoClient } = require('mongodb');
+const { connection } = require('./connection');
 
-const OPTIONS = {
-  useNewUrlParser: true, useUnifedTopology: true,
+const getAll = async () => {
+  const db = await connection();
+  const result = await db.collection('ceps').find().toArray;
+  return result;
 };
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-const DB_NAME = 'ceps';
-let db = null;
-
-const connection = async () => {
-  if (db) Promise.resolve(db);
-  const conn = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
-  db = conn.db(DB_NAME);
-  return db;
+module.exports = {
+  getAll,
 };
-
-module.exports = { connection };
