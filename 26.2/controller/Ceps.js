f52e41e161adc21c18995fc05a1e1076728bf992
lucas-chave <lcchavesr150@gmail.com>
2021-10-11 17:30:48 -0300

    correction exports

--- 26.2/controller/Ceps.js
+++ 26.2/controller/Ceps.js
@@ -1,10 +1,14 @@
 const { getAllCeps } = require('../service/Ceps');
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
   try {
     const ceps = await getAllCeps();
     res.status(200).json(ceps);
   } catch (error) {
     res.status(500).json({ message: 'Aconteceu um erro ao buscar os dados no servidor' });
   }
+};
+
+module.exports = {
+  getAll,
 };
