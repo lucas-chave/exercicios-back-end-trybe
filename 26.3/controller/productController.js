3442021df656eedf092230a195f7b70990114f76
lucas-chave <lcchavesr150@gmail.com>
2021-10-13 17:52:39 -0300

    adding success status code

--- 26.3/controller/productController.js
+++ 26.3/controller/productController.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get('/produtos', async (_req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.json(products);
+  res.status(200).json(products);
 });
 
 router.get('/produto/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.json(product);
+  res.status(200).json(product);
 });
 
 router.post('/usuario', async (req, res) => {
@@ -20,13 +20,13 @@ router.post('/usuario', async (req, res) => {
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.json(newProduct);
+  res.status(201).json(newProduct);
 });
 
 router.delete('/usuario/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.json(products);
+  res.status(200).json(products);
 });
 
 router.put('/usuario/:id', async (req, res) => {
@@ -34,7 +34,7 @@ router.put('/usuario/:id', async (req, res) => {
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.json(products);
+  res.status(200).json(products);
 });
 
 module.exports = router;
